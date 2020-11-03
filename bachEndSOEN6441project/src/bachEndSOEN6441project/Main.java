1bfc27897164e01a1495056029eaa01974dece6a
Arash <arash_mem@yahoo.com>
2020-11-03 03:34:04 -0500

    Updated API key and some minor issues

--- bachEndSOEN6441project/src/bachEndSOEN6441project/Main.java
+++ bachEndSOEN6441project/src/bachEndSOEN6441project/Main.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
+		int count=1;
 		String keyWords = "Montreal";
 		ArrayList<String> tweets = TweetManager.getTweets(keyWords);
 	
 		for(String tweet : tweets) {
-			System.out.println(tweet + " : " + tweet);
+			System.out.println("Tweet "+ count+ " : " + tweet);
+			count++;
 		}
 	}
 }
